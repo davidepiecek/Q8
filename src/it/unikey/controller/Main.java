43d2379bef763bfbcde355076af7ed508768eb84
davide_piecek <dado.pck@hotmail.it>
2022-01-31 16:40:13 +0100

    creazione package prima

--- src/it/unikey/controller/Main.java
+++ src/it/unikey/controller/Main.java
@@ -1,10 +1,10 @@
-package it.unikey;
+package it.unikey.controller;
 
 public class Main {
 
     public static void main(String[] args) {
 	// Prima prova sui push
-        
+
         int b = 67;
 
         System.out.println(b);
